333277e627fd4ee7af6163296361a6ce2ff0d222
= <=>
2023-12-08 17:47:01 +0000

    remove prefix url support

--- lib/i18n.rb
+++ lib/i18n.rb
@@ -6,15 +6,5 @@ module I18n
       locale = header.scan(/^[a-z]{2}/).first.to_sym
       available_locales.include?(locale) ? locale : default_locale
     end
-
-    # Prefix a url with the current locale
-    # Example: "/orders/show/1" becomes "/de/orders/show/1"
-    def link(url, text)
-      %(<a href="/#{locale}#{url}">#{t text}</a>)
-    end
-
-    def url(url)
-      "/#{locale}#{url}"
-    end
   end
 end
